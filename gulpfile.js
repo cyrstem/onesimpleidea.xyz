1dd5c9a868a4470345af3aa8fadacdc6d7333ba3
cyrstem <cyrstem@gmail.com>
2015-12-29 22:33:19 -0500

    new file:   app/css/style.min.css 	modified:   app/index.html 	new file:   app/js/main.min.js 	modified:   css/style.css 	modified:   gulpfile.js 	modified:   index.html 	modified:   js/main.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@
 // Setup and Varibles
 //------------------------------------
 var gulp = require('gulp');
-var mainBowerFiles = require('main-bower-files');
+	browserSync = require('browser-sync');
+	reload =  browserSync.reload;
+	plumber = require('gulp-plumber');
+	minify = require('gulp-minify-css');
+	uglify = require('gulp-uglify');
+	rename = require('gulp-rename');
+	concat = require('gulp-concat');
 
 //------------------------------------
 // Bower move 
@@ -10,25 +16,65 @@ var mainBowerFiles = require('main-bower-files');
 
 //------------------------------------
 // Third party libs boostrap&jquery concat
-//------------------------------------
+//------------------------------------ 
+gulp.task('build', function(){
+	gulp.src([
+		'third-party/bootstrap/dist/js/*.js',
+		'third-party/jquery/dist/*.js',
+		'third-party/p5.js/lib/*.js'
 
+		])
+	.pipe(plumber())
+	// .pipe(concat('app.js'))
+	.pipe(gulp.dest('./app/third-party'));
+
+	return gulp.src('third-party/bootstrap/dist/css/bootstrap.min.css')
+	.pipe(gulp.dest('./app/css'));
+
+});
 //------------------------------------
 // Personal css 
 //------------------------------------
-
+gulp.task('minifyCss',function(){
+	gulp.src('css/*.css')
+		.pipe(plumber())
+		.pipe(minify())
+		.pipe(rename({suffix:'.min'}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(reload({stream:true}));
+});
 //------------------------------------
 // JS P5js  mio 
 //------------------------------------
-
-
+gulp.task('scripts',function(){
+	gulp.src('js/main.js')
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(rename({suffix:'.min'}))
+		.pipe(gulp.dest('./app/js'))
+		.pipe(reload({stream:true}));
+});
 //------------------------------------
 // Browser-Sync and html change reload
 //------------------------------------
+gulp.task('browser-sync', function(){
+	browserSync({
+		server:{
+			baseDir:"./app"
+		}
+	});
+	 gulp.watch("app/*.html").on("change", browserSync.reload);
+});
 
 //------------------------------------
 // Watch task
 //------------------------------------
-
+gulp.task('watch', function(){
+	gulp.watch('css/*.css',['minifyCss']);
+	gulp.watch('js/*.js',['scripts']);
+	gulp.watch('app/*.html');
+});
 //------------------------------------
 // Main task
 //------------------------------------
+gulp.task('default',['browser-sync','minifyCss','scripts','watch']);
