ddddd7e2fa3ee628783bdf5d2027a037a288f085
cyrstem <cyrstem@gmail.com>
2016-01-12 16:54:00 -0500

    modified:   app/index.html 	modified:   app/js/main.min.js 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 var scaler = 200;
+var value =360;
+var d =0;
 var c0;
 function setup() { 
+  //var myCanvas = createCanvas (windowWidth,windowHeight);
   var myCanvas = createCanvas (800,800);
   myCanvas.parent('paint');
   noFill();
   stroke(0);
-  c0 = new esfera(width / 2, height / 2);
+  c0 = new esfera(width/ 2, height/ 2);
 }
 
 function draw() {
   background(255);
   c0.display();
   c0.update();
-
 }
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
 
 function esfera(x, y) {
   this.x = x;
@@ -23,14 +28,15 @@ function esfera(x, y) {
   this.n2 = 1;
   this.n3 = 10;
   this.display = function() {
-    drawShape(width / 2, height / 2,this.m,this.n1,this.n2,this.n3);
+    //drawShape(windowWidth/3, windowHeight/2,this.m,this.n1,this.n2,this.n3);
+    drawShape(width/2, height/2,this.m,this.n1,this.n2,this.n3);
   }
   this.update = function() {
     this.m = random(1,5);
     this.n1 = map(mouseY, 0, height, 0, 180);
     this.n2 = map(mouseX, 0, width, 0, 180);
     this.n3 = noise(0,0.55);
-    fill(4);
+    fill(d);
     stroke(random(100,255));
     strokeWeight(random(0.1,0.6));
   }
@@ -59,8 +65,18 @@ function drawShape(x, y, m, n1,n2,n3) {
   pop();
 }
 
+function mousePressed() {
+  if (d == 0) {
+
+    d =random(0,255);
+
+  } else {
+    d = 0;
+  }
+}
+
 function superformula(m, n1, n2, n3) {
-  var numPoints = 360;
+  var numPoints = value;
   var phi = TWO_PI / numPoints;
   var points = []
   for (var i = 0; i <= numPoints; i++) {
