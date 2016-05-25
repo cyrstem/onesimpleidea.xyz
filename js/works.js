46822cf39f839e13d5f266ae98cf2074c62527df
cyrstem <cyrstem@gmail.com>
2016-05-25 14:02:18 -0500

    modified:   app/js/works.min.js 	modified:   app/work.html 	modified:   js/works.js

--- js/works.js
+++ js/works.js
@@ -12,7 +12,6 @@ function inicio(){
 function misCosas(){
  console.log("./Loading Proyectos");
     $(".work").load("work.html").prependTo("body");
-    //$(".work").load("work.html").append('body');
     $('html').css("overflow-y","scroll");
 
     isoTest();  
@@ -21,25 +20,40 @@ function misCosas(){
 function isoTest(){
 console.log("./listo -fin creo ojala ya nose qmas hacer ");
 $('body').css("background-color","000000");
- var $container = $('.portfolioContainer');
-    $container.isotope({
-        filter:'*',
-        animationOptions:{
-            duration:750,
-            easing:'linear',
-            queue:false
-        }
-    });
-
-$('.portfolioFilter a').on("click",function(){
-    console.log("hello");
-});
-
 }
 
 $( window ).scroll(function() {
     $('center').remove();
 });
 
+function listo(){
+var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+});//load init
+
+$('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    }); //end filter lit
+}
+
+
 window.onload = inicio;
+window.ready = listo;
 
