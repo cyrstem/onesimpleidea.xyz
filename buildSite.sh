7a506dfc43523b5e1f0b3411680c50a68e758b0d
cyrstem <cyrstem@gmail.com>
2017-04-04 11:10:41 -0500

    new file:   buildSite.sh 	new file:   testFolder/hello.md

---
+++ buildSite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This is to build site on Github by coping the app folder and replace the content on docs files so the site can be public#
+echo "# onesimpleidea publish#"
+echo "# Que archivo o carpeta cambio"
+read input_variable
+cd docs
+rm -r $input_variable
+echo "Remove  el archivo anterior"
+echo "pasando tus huevadas"
+cp -r $input_variable docs/$input_variable
+echo "Done BIAtch done"
+
