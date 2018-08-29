19e472f095438068f8b25516e54126a507db0387
cyrstem <cyrstem@gmail.com>
2018-08-28 21:47:23 -0500

    new file:   app/assets/3.png 	modified:   app/css/main.css 	modified:   app/index.html 	modified:   app/js/works.min.js 	modified:   js/works.js 	modified:   sass/_content.scss

--- js/works.js
+++ js/works.js
@@ -1,10 +1,29 @@
 
  new hoverEffect({
-  parent: document.querySelector('.info-1'),
+  parent: document.querySelector('.info'),
   intensity1: 0.1,
   intensity2: 0.1,
   angle2: Math.PI / 2,
   image1: 'assets/annihilation1.jpg',
   image2: 'assets/annihilation2.jpg',
   displacementImage: 'assets/1.jpg'
+});
+  new hoverEffect({
+  parent: document.querySelector('.info-2'),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1: 'assets/instalations.jpg',
+  image2: 'assets/instalations.jpg',
+  displacementImage: 'assets/2.jpg'
+});
+
+  new hoverEffect({
+  parent: document.querySelector('.info-3'),
+  intensity1: 0.1,
+  intensity2: 0.1,
+  angle2: Math.PI / 2,
+  image1: 'assets/opencv1.jpg',
+  image2: 'assets/opencv1.jpg',
+  displacementImage: 'assets/3.png'
 });
