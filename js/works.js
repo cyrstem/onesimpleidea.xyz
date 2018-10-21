54da994987ba93c2978e78cd93afefa4e751257e
cyrstem <cyrstem@gmail.com>
2018-10-21 18:30:01 -0500

    new file:   app/assets/imagesLoad/annihilation1.png 	new file:   app/assets/imagesLoad/annihilation2.png 	new file:   app/assets/imagesLoad/annihilation3.png 	modified:   app/css/main.css 	modified:   app/index.html 	modified:   app/js/works.min.js 	modified:   js/works.js 	modified:   sass/_base.scss 	modified:   sass/_content.scss 	modified:   sass/_layout.scss 	modified:   sass/_nav.scss 	new file:   sass/_whoami.scss

--- js/works.js
+++ js/works.js
@@ -4,8 +4,8 @@
   intensity1: 0.1,
   intensity2: 0.1,
   angle2: Math.PI / 2,
-  image1: 'assets/annihilation1.jpg',
-  image2: 'assets/annihilation2.jpg',
+  image1: 'assets/imagesLoad/annihilation1.png',
+  image2: 'assets/imagesLoad/annihilation2.png',
   displacementImage: 'assets/4.jpg'
 });
   new hoverEffect({
