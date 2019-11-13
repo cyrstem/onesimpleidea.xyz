11967a37627869bff4605ba0018d4d59eb9fcb13
jacos <cyrstem@gmail.com>
2019-11-12 21:06:51 -0500

    deleted:    .cache/3a/6031cb2e18829ad8b119fd288f7017.json 	deleted:    .cache/40/b6b050fb7969af682ead99dba44d07.json 	deleted:    .cache/45/6fb5dfe545f4aebbe75a33379b8ac8.json 	deleted:    .cache/4f/ff065ea5cebe742422a8e850743e62.json 	deleted:    .cache/51/7451263a5e84a3f5c6f9178458e6ef.json 	deleted:    .cache/61/7dd0dfa50c6a062c8a559ad65934d5.json 	deleted:    .cache/62/9c2a99f9f0681ecb8a9f4e3195baf9.json 	deleted:    .cache/70/fdd6c173fcb515e9d85755f4372b1f.json 	deleted:    .cache/7a/261b884a2bff2c75ca4e99cf2eb64a.json 	deleted:    .cache/7c/a928043e2f1b166a21fecb08cc3090.json 	deleted:    .cache/88/d20abb820f4c6d6146ed8d00c7744e.json 	deleted:    .cache/c0/e3693d806e24c6cd0fb54c954bcf51.json 	deleted:    .cache/c3/bc597710f8d5cedd7792399dd2f342.json 	deleted:    .cache/c4/db563c27a1a77ddbbdd7f0997c0994.json 	deleted:    .cache/ee/87a8535c9bec7ed1062ede199366ad.json 	deleted:    .cache/fc/c96c85eb8b6d1542b974366787b3c3.json 	modified:   dist/index.html 	new file:   dist/src.4d3da907.js 	new file:   dist/src.4d3da907.js.map 	new file:   dist/src.b36bfe52.css 	new file:   dist/src.b36bfe52.css.map 	modified:   dist/src.e31bb0bc.js 	modified:   dist/src.e31bb0bc.js.map

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -45409,7 +45409,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46743" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45269" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
