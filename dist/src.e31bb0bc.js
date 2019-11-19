2f61de9865641638eda026d97345648f05f8d685
jacos <cyrstem@gmail.com>
2019-11-18 19:53:08 -0500

    modified:   dist/src.e31bb0bc.css 	modified:   dist/src.e31bb0bc.css.map 	modified:   dist/src.e31bb0bc.js 	modified:   dist/src.e31bb0bc.js.map 	new file:   src/assets/texture.jpg 	modified:   src/stage.js 	modified:   src/style/_ui.scss

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -45261,8 +45261,9 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 //colors setup 
 var colors = [0x141e30, 0x243b55, 0xffffff, 0x4F5B66, 0x0CE5DB, 0x00000];
 var scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x3f7b9d, 0, 60);
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 10;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xffffff);
@@ -45277,20 +45278,24 @@ window.addEventListener('resize', function () {
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2(); //element
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshPhongMaterial({
-  color: colors[0],
-  aoMapIntensity: 0.9,
-  emissive: colors[7],
-  emissiveIntensity: 0.4,
-  reflectivity: 0.4,
-  shininess: 15,
-  wireframe: false
-});
+var texture = new THREE.TextureLoader().load("assets/texture.jpg");
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(2, 2);
+var geometry = new THREE.BoxGeometry(1, 1, 1); //const material2 = new THREE.LineDashedMaterial({color:0x2d2727,linewidth:0.2,scale:0.4,dashSize:2,gapSize:0.3});
+
+var material3 = new THREE.MeshMatcapMaterial({
+  color: 0x160000,
+  transparent: false,
+  opacity: 0.9,
+  depthTest: true,
+  depthWrite: true
+}); //const material = new THREE.MeshPhongMaterial({color :colors[0],aoMapIntensity: 0.9,emissive:colors[7],emissiveIntensity :0.4,reflectivity:0.4,shininess:15,wireframe:false} );
+
 var meshS = -100;
 
 for (var i = 0; i < 250; i++) {
-  var mesh = new THREE.Mesh(geometry, material);
+  var mesh = new THREE.Mesh(geometry, material3);
   mesh.position.x = (Math.random() - 0.5) * 20;
   mesh.position.y = (Math.random() - 0.5) * 10;
   mesh.position.z = (Math.random() - 0.5) * 10;
@@ -45298,25 +45303,26 @@ for (var i = 0; i < 250; i++) {
   meshS += 12;
 } // mesh.position.x = 2;
 //lights
+// var hemi = new THREE.HemisphereLight( 0xffffbb, 0x080820, 3 );
+// scene.add( hemi );
+// let light = new THREE.PointLight( 0xff0000, 0.8, 100 );
+// light.position.set(0,-10,-25);
+// scene.add(light);
+// let light1 = new THREE.PointLight( 0xFFFFFF, 0.7, 5000,2 );
+// //light1.position.set(0,0,0);
+// scene.add(light1);
 
 
-var hemi = new THREE.HemisphereLight(0xffffbb, 0x080820, 3);
-scene.add(hemi);
-var light = new THREE.PointLight(0xff0000, 0.8, 100);
-light.position.set(0, -10, -25);
-scene.add(light);
-var light1 = new THREE.PointLight(0xFFFFFF, 0.7, 5000, 2); //light1.position.set(0,0,0);
-
-scene.add(light1);
+var ambient = new THREE.AmbientLight(0x0);
+scene.add(ambient);
 
 var render = function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera);
   var time = Date.now();
-  0.005;
-  light1.position.x = Math.sin(time * 0.7) * 300;
-  light1.position.y = Math.cos(time * 0.5) * 40;
-  light1.position.z = Math.cos(time * 0.3) * 30;
+  0.005; // light1.position.x = Math.sin( time * 0.7 ) * 300;
+  // light1.position.y = Math.cos( time * 0.5 ) * 40;
+  // light1.position.z = Math.cos( time * 0.3 ) * 30;
 }; // render();
 // var tl = new TimelineMax().delay(.3);
 // 	tl.to(mesh.scale,1,{x:7, ease: Expo.easeOut})
@@ -45403,7 +45409,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40469" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38389" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
