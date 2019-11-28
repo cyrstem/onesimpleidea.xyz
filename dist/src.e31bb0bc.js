870ce5f7938572deb228b8cb90aa81f1d011deac
jacos <cyrstem@gmail.com>
2019-11-28 14:05:54 -0500

    deleted:    dist/src.660f1bd8.js 	deleted:    dist/src.660f1bd8.js.map 	modified:   dist/src.e31bb0bc.js

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -45420,7 +45420,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41841" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40361" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
