b7fad7041672e13bbf776954aef655650521fa72
jacos <cyrstem@gmail.com>
2020-06-17 17:57:33 -0500

    continuee

--- src/js/Home.js
+++ src/js/Home.js
@@ -4,10 +4,8 @@ const Home =()=>{
             <h1>Hello..</h1>
             <p>I'm <b>Jacob</b> a <b>Creative Developer and Programmer from Quito - Ecuador.</b></p> 
             <p>I specialize in working with emerging tech to build custom tools for digital or physical experiences.</p>
-            <p>Im a self-taugh developer, fast learner and recently curious</p>
-            <p>about Machine Learning.</p>
-            <p>I have work as a Front-End developer for more than 5 years</p>
-            <p>on different companies from E-commerce sites to private Industry</p>
+            <p>Im a self-taugh developer, fast learner and recently curious about Machine Learning.</p>
+            <p>I have work as a Front-End developer for more than 5 years on different companies from E-commerce sites to private Industry</p>
             <p> contact me at <b>cyrstem[at]gmail.com</b>.</p>   
         </main>
     `;
