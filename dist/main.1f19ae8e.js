de7d174c1226632e75ef189a5914238a191ec9a9
jacos <cyrstem@gmail.com>
2020-11-25 20:29:04 -0500

    fixes

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -46502,7 +46502,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42143" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41549" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
