6a73036796c3deacf9620b9851962593647ff963
jacos <cyrstem@gmail.com>
2021-02-09 20:08:23 -0500

    quick fixes

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -6,7 +6,7 @@ const Home =()=>{
             <p>I specialize in working with emerging tech to build custom tools for digital or physical experiences.</p>
             <p>Im a self-taught developer, fast learner and recently curious about Machine Learning.</p>
             <p>I have worked as a Front-End developer for more than 5 years on different companies from E-commerce sites to private Industry</p>
-            <p> contact me at <b>cyrstem[at]gmail[dot]com</b>.</p>   
+            <p> contact me at <b>cyrstem[at]gmail[dot]com</b></p>   
         </main>
     `;
     return template;
