ea9d60026d8e9a6f6dc669a3ec77b6be7b10b6b2
jacos <cyrstem@gmail.com>
2021-02-09 19:56:10 -0500

    added links

--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -7,6 +7,7 @@ const Portafolio =()=>{
             <li><a href="yaesta.com">YaEsta.com = Frontend Developer and Lead Designer (2016-2018)</a></li>
             <li><a href="todo.legal">Todo.legal = Frontend Developer and Unity Developer (2020-2021)</a></li>
             <li><a href="https://smartco.com.ec">Smartco = Frontend Developer and Unity Developer (2018-2020)</a></li>
+            <li><a href="https://www.pachamama.org.ec/en/">Pachamama Alliance = Front-end Developer and Lead Designer (2010–2013)</a></li>
          </ul>
       </div>
     `;
