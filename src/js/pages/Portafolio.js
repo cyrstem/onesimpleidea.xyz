6bdbc7a4a43885fd553741087535488a1af83782
jacos <cyrstem@gmail.com>
2021-02-10 18:44:51 -0500

    rest

--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -4,8 +4,8 @@ const Portafolio =()=>{
       <div class ="Sites">
          <h1>I worked as Front-End developer and Lead Designer for the following companies:  </h1>
          <ul>
-            <li><a href="https://yaesta.com" target="_blank">YaEsta.com = Frontend Developer and Lead Designer (2016-2018)</a></li>
-            <li><a href="https://todo.legal" target="_blank">Todo.legal = Frontend Developer and Unity Developer (2020-2021)</a></li>
+            <li><a href="https://www.yaesta.com" target="_blank">YaEsta.com = Frontend Developer and Lead Designer (2016-2018)</a></li>
+            <li><a href="https://todo.legal" target="_blank">Todo.legal = Frontend Developer and Web Developer(2020-2021)</a></li>
             <li><a href="https://smartco.com.ec" target="_blank">Smartco = Frontend Developer and Unity Developer (2018-2020)</a></li>
             <li><a href="https://www.pachamama.org.ec/en/" target="_blank">Pachamama Alliance = Front-end Developer and Lead Designer (2010–2013)</a></li>
          </ul>
