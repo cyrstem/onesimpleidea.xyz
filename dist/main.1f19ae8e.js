cdaf4b7587c9172a6a517ebd56cc12b5719afad9
cyrstem <cyrstem@gmail.com>
2021-08-03 19:26:24 -0500

    modified:   dist/main.1f19ae8e.js 	deleted:    package-lock.json 	new file:   yarn.lock

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -41259,7 +41259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40863" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41539" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
