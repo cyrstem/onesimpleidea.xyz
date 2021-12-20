62e6c311b2d060e328a61cd502d6c1e23b2d204e
jacob <jacob@activetheory.net>
2021-12-20 14:16:44 -0500

    modified:   src/js/pages/Home.js

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,14 +1,15 @@
 const Home = () => {
-	const template = `
+    const template = `
         <main>
             <h1>Hello..</h1>
             <p>I'm <b>Jacob</b> a <b>Creative Developer</b> and <b>Designer</b> from Quito - Ecuador.</p> 
             <p>I specialize in working with emerging tech to build custom tools for digital or physical experiences.</p>
+            <p> Currently working as a webGL developer at <a href="activetheory.net" target ="_blank"> ActiveTheory</a></p>
             <p>Im a <b>self-taught</b> developer, <b>fast learner</b> and recently curious about <b>Machine Learning.</b></p>
             <p>I have worked as a <b>Front-End</b> developer for more than 5 years on different companies from E-commerce sites to private Industry</p>
             <p> contact me at <b>cyrstem[at]gmail[dot]com</b></p>   
         </main>
     `;
-	return template;
+    return template;
 };
 export default Home;
