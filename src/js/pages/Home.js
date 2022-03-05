24a4f497c93515df5d3ab57744052d8e84f38578
cyrstem <cyrstem@gmail.com>
2022-03-04 21:34:34 -0500

    rework media querys , update proyects add info and clean some  extras from work

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,10 +1,11 @@
 const Home = () => {
     const template = `
-    <h1>Hello..</h1>
+    
         <main>
+        <h1>Hello..</h1>
             <p>I'm <b>Jacob</b> a <b>Creative Developer</b> and <b>Front-End Developer</b> based in Quito - Ecuador.</p> 
-            <p>I specialize building custom digital or physical experiences.</p>
-            <p>Im a <b>self-taught</b> developer, <b>fast learner</b> that works with<b> WebGL, JS, C++, OpenGL, GLSL</b></p>
+            <p>I specialize building custom digital or physical experiences,</p>
+            <p><b>self-taught</b> developer, <b>fast learner</b> that works with<b> WebGL, JS, C++, OpenGL, GLSL</b></p>
             <p>and recently curious about <b>Machine Learning.</b></p>
             <p> contact me at <b>cyrstem[at]gmail[dot]com</b></p>   
         </main>
--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -13,31 +13,25 @@ const Portafolio = () => {
             <a href="https://myuniguru.com/" target="_blank">
               <span>My Uniguru</span>
               <span>FullStack Developer</span>
-              <span class="number">2020 - 2021</span>
+              <span class="number">2020</span>
             </a>
           </li>
-         
+          <li>
+          <a href="https://smartco.com.ec" target="_blank"> 
+            <span>Smartco </span>
+            <span>Unity Developer</span>
+            <span class="number">2019 - 2020</span>
+          </a>
+        </li>
           <li>
             <a href="https://www.yaesta.com" target="_blank">
               <span> YaEsta </span>
-              <span> Front-end Developer & Designer</span> 
+              <span> Front-end  & Designer</span> 
               <span class="number">2016 - 2018</span>
             </a>
           </li>
-          <li>
-            <a href="https://todo.legal" target="_blank">
-              <span>Todo.legal</span> 
-              <span>Front-end Developer & Web Designer</span>
-              <span class="number">2020 - 2020</span>
-            </a>
-          </li>
-          <li>
-            <a href="https://smartco.com.ec" target="_blank"> 
-              <span>Smartco </span>
-              <span> Front-end Developer & Unity Developer</span>
-              <span class="number">2019 - 2020</span>
-            </a>
-          </li>
+          
+      
           <li>
             <a href="https://www.pachamama.org.ec/en/" target="_blank">
               <span> Pachamama</span>
