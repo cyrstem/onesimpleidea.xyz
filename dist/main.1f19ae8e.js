18a662e559d6e7945940afb94dde74014841d5fb
cyrstem <cyrstem@gmail.com>
2022-03-05 12:42:33 -0500

    yup responsiove shits

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -9893,7 +9893,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32919" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36549" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
