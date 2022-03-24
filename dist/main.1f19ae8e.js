694669567a920e32bd56688d33e21c3ebfff5667
cyrstem <cyrstem@gmail.com>
2022-03-24 09:01:55 -0500

    fixed build

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -10169,7 +10169,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44277" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43213" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
