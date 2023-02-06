c32e8f8e3cecf33c5040d7532fbcf0bd81e4fc9b
cyrstem <cyrstem@gmail.com>
2023-02-06 18:47:02 -0500

    vite clean up

--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,59 +1,6 @@
 
 const Home = () => {
     const template = `
-<<<<<<< HEAD
-        <main>
-        <p>Hello, I'm building something new so in the meantime....</p>
-        <p>here is everything you need to know about me.</p>
-        <p>I'm <b>Jacob</b> a <b>Creative Developer</b> and <b>Front-End Developer</b> based in Quito - Ecuador, specialized in building custom digital or physical experiences.</p> 
-        <p><b>Self-taught</b> developer, <b>fast learner</b> that works with <b> WebGL, JS, C++, OpenGL, GLSL </b>and recently curious about <b>Machine Learning.</b></p>
-        <div id ="sites"> 
-        <p> I have collaborated with:</p>
-        <ul>
-        <li>
-        <a href="https://activetheory.net/" target="_blank">
-          <span>Active Theory</span> 
-          <span> --- WebGL Developer</span>
-        </a>
-       </li>
-        <li>
-         <a href="https://visualgoodness.com/" target="_blank">
-           <span>Visual Goodness </span> 
-           <span> --- WebGL Developer</span>
-         </a>
-        </li>
-        <li>
-           <a href="https://myuniguru.com/" target="_blank">
-             <span>My Uniguru</span>
-             <span> --- FullStack Developer</span>
-           </a>
-         </li>
-         <li>
-         <a href="https://smartco.com.ec" target="_blank"> 
-           <span>Smartco </span>
-           <span> --- Unity Developer</span>
-         </a>
-       </li>
-         <li>
-           <a href="https://www.yaesta.com" target="_blank">
-             <span> YaEsta </span>
-             <span> --- Front-end  & Designer</span> 
-           </a>
-         </li>
-        </ul>
-     </div>
-     <p>Contact me at <b>cyrstem[at]gmail[dot]com</b></p>  
-    
-    <div id="links">
-     <ul>
-     <a href ="https://www.instagram.com/cyrstem/"target="_blank"><img src="insta.png" width="20"></a>
-         <a href ="http://ec.linkedin.com/in/jacobohz" target="_blank"><img src="in.png" width="20"></a>
-     <a href ="https://github.com/cyrstem/" target="_blank"><img src="git.png" width="20"></a>
-    </ul>
-    </div>
-    
-        </main>
-=======
     <main>
         <div id="content">
             <p>Hello...</p>
@@ -92,7 +39,6 @@ const Home = () => {
         
         </div>
     </main>
->>>>>>> dev
     `;
     return template;
 };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 
 import "./style/main.scss";
-<<<<<<< HEAD
-import lines from "./js/follow";
-import GUIView from "./js/app";
-
-const app = () => {
-	let  ui = new GUIView();
-	ui.init();
-	lines();
-};
-=======
 import simpleSign from './js/utils/Sign'
 import App from './js/app'
->>>>>>> dev
 
 window.onload = (event) => {
 	new App(
