a77baf0dcabbef038937f09fe7be0809de597f2f
cyrstem <cyrstem@gmail.com>
2023-02-15 22:49:39 -0500

    pulse activated

--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -4,7 +4,7 @@ const Portafolio = () => {
     const template = `
     <main>  
         <div id="portafolio">
-            <div><img src="poster4.jpg"/></div>
+            <div><img src="poster5.jpg"/></div>
             <div><img src="insta-0.jpg"/></div>
             <div><img src="noizu.png" /></div>
             <div><img src="insta-3.jpg"/></div>
@@ -13,5 +13,5 @@ const Portafolio = () => {
     `;
     return template;
 };
-
+    
 export default Portafolio;
