ccd37a8c0ad7095ede74c239e26fae272a7dd60b
cyrstem <cyrstem@gmail.com>
2023-02-16 20:01:41 -0500

    fixed horizontal

--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -4,7 +4,19 @@ const Portafolio = () => {
     const template = `
     <main>  
         <div id="portafolio">
-            <div><img src="poster5.jpg"/></div>
+            <div>
+                <article>
+                    <h2>Moving Photon</h2>
+                    <p>I collaborated on developing and deploying the Virtual Experience for
+                        <a href="https://friendred.studio/2021/10/07/moving-photon/" target="_blank">Moving Photon</a> an
+                        interactive installation/performance
+                        created by installation artist<a href="https://friendred.studio" target="_blank"> Friendred Peng.</a>
+                        Participation in Moving Photon can be in 5 different ways, including a Phantom performance,
+                        interactive installation, interactive performance,interactive performance with EEG and a <a
+                            href="https://movingphoton.friendred.studio/" target="_blank"> remote performance.</a>
+                </article>
+                <img src="poster5.jpg"/>
+            </div>
             <div><img src="insta-0.jpg"/></div>
             <div><img src="noizu.png" /></div>
             <div><img src="insta-3.jpg"/></div>
