1bcc2876995ebf053db72b88157a8fbb2a2e71ef
cyrstem <cyrstem@gmail.com>
2023-02-17 12:34:58 -0500

    modified:   src/js/pages/Portafolio.js 	modified:   src/style/_layout.scss

--- src/js/pages/Portafolio.js
+++ src/js/pages/Portafolio.js
@@ -3,23 +3,23 @@
 const Portafolio = () => {
     const template = `
     <main>  
-    <div id="portafolio">
-    <div>
-        <img src="poster5.jpg" />
-    </div>
-    <div>
-        <img src="insta-0.jpg" />
-    </div>
-    <div>
-        <img src="noizu.png" />
-    </div>
-    <div>
-        <img src="insta-1.jpg" />
-    </div>
-    <div>
-    <img src="insta-2.jpg" />
-    </div>
-    </div>
+        <div id="portafolio">
+            <div>
+                <img src="poster5.jpg" />
+            </div>
+            <div>
+                <img src="insta-0.jpg" />
+            </div>
+            <div>
+                <img src="noizu.png" />
+            </div>
+            <div>
+                <img src="insta-1.jpg" />
+            </div>
+            <div>
+            <img src="insta-2.jpg" />
+            </div>
+        </div>
     </main>
     `;
     return template;
